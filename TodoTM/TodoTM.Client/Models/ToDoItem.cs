c7df77f2d039f5f4953d6c6b31dcb3e63d9302fa
Tra My Ngo <myntt1610@outlook.com>
2025-08-15 02:14:56 +0700

    Implement List Page

--- TodoTM/TodoTM.Client/Models/ToDoItem.cs
+++ TodoTM/TodoTM.Client/Models/ToDoItem.cs
@@ -2,5 +2,17 @@
 {
     public class ToDoItem
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Status { get; set; } // True = Done, False = Not Done
+        public CategoryType Category { get; set; }
+        public DateTime? Deadline { get; set; }
+    }
+
+    public enum CategoryType
+    {
+        None,
+        Personal,
+        Business
     }
 }
